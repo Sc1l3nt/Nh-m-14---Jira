757ba5a8f5bd77d8eafcfa5f89cc62d0ab7026f0
tminhbao <tminhbao2001@gmail.com>
2023-01-19 01:12:23 +0700

    handle login

--- src/Redux/Reducers/userReducer.tsx
+++ src/Redux/Reducers/userReducer.tsx
@@ -1,15 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { http } from "../../Utils/config";
+import { DispatchType } from "../configStore";
+import { history } from "../../index";
+
+export interface UserLoginModel {
+  email: string;
+  password: string;
+}
+
+export interface UserRegisterModel {
+  email: string;
+  password: string;
+  name: string;
+  phoneNumber: string;
+}
 
 const initialState = {
   userLogin: null,
+  userProfile: null,
 };
 
 const UserReducer = createSlice({
   name: "UserReducer",
   initialState,
-  reducers: {},
+  reducers: {
+    loginAction: (state, action) => {
+      state.userLogin = action.payload;
+    },
+    registerAction: (state, action) => {
+      state.userProfile = action.payload;
+    },
+  },
 });
 
-export const {} = UserReducer.actions;
+export const { loginAction, registerAction } = UserReducer.actions;
 
 export default UserReducer.reducer;
+
+export const loginApi = (userLogin: UserLoginModel) => {
+  return async (dispatch: DispatchType) => {
+    const result = await http.post("/Users/signin", userLogin);
+    const action = loginAction(result.data.content);
+    dispatch(action);
+    history.push("/");
+  };
+};
