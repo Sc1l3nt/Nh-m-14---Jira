4c0a3ff70296c87fde2bd3f46b1af956ef2ceb25
tminhbao <tminhbao2001@gmail.com>
2023-01-19 10:25:02 +0700

    updateee

--- src/Components/FormInput.tsx
+++ src/Components/FormInput.tsx
@@ -7,7 +7,7 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 import { DispatchType } from "../Redux/configStore";
 import { useDispatch } from "react-redux";
-import { loginApi, registerApi } from "../Redux/Reducers/UserReducer";
+import { loginApi, registerApi } from "../Redux/Reducers/userReducer";
 
 type Props = {
   listRender: string[];
--- src/Pages/Project Management/Projectmanagement.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const Projectmanagement = (props: Props) => {
-  return <div style={{ height: window.innerHeight }}>Projectmanagement</div>;
-};
-
-export default Projectmanagement;
