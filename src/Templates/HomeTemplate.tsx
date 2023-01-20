bd0e4390cb237e281e19d8abd9720f2ecb84ade2
Truong <truongdai148635@gmail.com>
2023-01-20 19:36:30 +0700

    DAI layout TemplateMobile

--- src/Templates/HomeTemplate.tsx
+++ src/Templates/HomeTemplate.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { SizeType } from "antd/es/config-provider/SizeContext";
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
 import "./HomeTemplate.scss";
+import { FloatButton } from "antd";
 
 type Props = {};
 
 const HomeTemplate = (props: Props) => {
-  const [size, setSize] = useState<SizeType>();
   return (
     <div className="template">
       <Header />
-      <div className="creat-button" style={{ position: 'sticky', top: '700px', marginLeft: '1410px', height: '20px' }}>
-        <button className="rounded-circle bg-primary text-light fs-4 btn py-1">
-          <i className="fa-solid fa-plus"></i>
-        </button>
-      </div>
-      <div className="d-flex my-2 mx-1">
+      <FloatButton
+      shape="circle"
+      type="primary"
+      style={{ right: 40 }}
+      icon={<i className="fa-solid fa-plus"></i>}
+    />
+      <div className="d-flex my-4 mx-1">
         <div className="item-left item">
           <Menu />
         </div>
